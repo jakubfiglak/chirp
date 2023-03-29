f7cc5885b889a44ac098a9f2457d9e035f22a97c
jakubfiglak <jakub.figlak@netguru.pl>
2023-03-29 19:32:03 +0200

    feat: add posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
+import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   const auth = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
+  console.log(data);
 
   return (
     <>
@@ -13,8 +17,15 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        {!!auth.user && <h3>Hello {auth.user?.username}</h3>}
-        {!auth.user ? <SignInButton /> : <SignOutButton />}
+        <div>
+          {!!auth.user && <h3>Hello {auth.user?.username}</h3>}
+          {!auth.user ? <SignInButton /> : <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
